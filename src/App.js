2ccaf9c76c4fd4768562cbba97f78f2c08c04e2f
“elizanort” <elizabethnortman@gmail.com>
2020-06-18 14:12:53 -0400

    started first prototype

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    month: "",
+    day: "",
+    year: "",
+
+    errors: {
+      oldAge: "",
+      notBorn: "",
+    },
+
+    zodiacs: {
+      capricorn: <img src="./img/capricorn.svg"/>,
+      aquarius: <img src="./img/capricorn.svg"/>,
+      pisces: <img src="./img/capricorn.svg"/>,
+      taurus: <img src="./img/capricorn.svg"/>,
+      gemini: <img src="./img/capricorn.svg"/>,
+      cancer: <img src="./img/capricorn.svg"/>,
+      leo: <img src="./img/capricorn.svg"/>,
+      virgo: <img src="./img/capricorn.svg"/>,
+      libra: <img src="./img/capricorn.svg"/>,
+      scorpio: <img src="./img/capricorn.svg"/>,
+      sagittarius: <img src="./img/capricorn.svg"/>,
+    }
+  };
+
+  handleChange = (event) => {
+    let name = event.target.name;
+    let value = event.target.value;
+    this.setState({[name]:value});
+
+    if (this.state.year)
+  };
+
+
+
+  render() {
+    return (
+      <div className="inputWrapper">
+        <div className="icon">
+          
+        </div>
+        <input
+          name="month"
+          className="input month"
+          value = {this.state.month}
+          onChange = {this.handleChange}
+          type="text"
+          placeholder="MM"
+          maxLength="2"
+          pattern="\d*"
+        />
+        <span>/</span>
+        <input
+          name="day"
+          className="input day"
+          value = {this.state.day}
+          onChange = {this.handleChange}
+          type="text"
+          placeholder="DD"
+          maxLength="2"
+          pattern="\d*"
+        />
+        <span>/</span>
+        <input
+          name="year"
+          className="input year"
+          value = {this.state.year}
+          onChange = {this.handleChange}
+          type="text"
+          placeholder="YYYY"
+          maxLength="4"
+          pattern="\d*"
+        />
+      </div>
+      <div>
+        Icons made by <a href="https://www.flaticon.com/authors/flat-icons" title="Flat Icons">Flat Icons</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+      </div>
+      
+    );
+  }
 }
 
 export default App;
